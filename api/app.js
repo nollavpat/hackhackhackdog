5b2a352774e9b7ecc9468f16e9395dd806f00f12
jcselorio <joemarie@ubx.ph>
2019-08-15 17:45:13 +0800

    finished calendar service

--- api/app.js
+++ api/app.js
@@ -1,8 +1,1 @@
 require('app-module-path').addPath(require('app-root-path').toString());
-
-const CalendarService = require('services/CalendarServices.js');
-
-
-const calendarService = new CalendarService();
-
-calendarService.authorize();
