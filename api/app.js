13440bbce6343d89dfc2a5b55f83e0682e07b220
jcselorio <joemarie@ubx.ph>
2019-08-16 05:11:29 +0800

    added get reservations and fix responses

--- api/app.js
+++ api/app.js
@@ -1,28 +1,28 @@
 require('app-module-path').addPath(require('app-root-path').toString());
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 const express = require('express');
 const HttpSuccess = require('api/responses/HttpSuccess.js');
 const app = express();
 
-// const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = ['http://localhost:3000'];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-// app.use(
-//     cors({
-//       origin: function(origin, callback) {
-//         if (!origin) return callback(null, true);
-//         if (allowedOrigins.indexOf(origin) === -1) {
-//           const msg =
-//           'The CORS policy for this site does not ' +
-//           'allow access from the specified Origin.';
-//           return callback(new Error(msg), false);
-//         }
-//         return callback(null, true);
-//       },
-//     })
-// );
+app.use(
+    cors({
+      origin: function(origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+          const msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      },
+    })
+);
 
 // ROUTERS
 app.use('/api/ror/', require('api/routers/ReservationsRouter'));
