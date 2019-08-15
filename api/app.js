7af16cd8e50808edf24acc7fa8b4f50f8de41801
Jairus Patrick Vallon <jairus@ubx.ph>
2019-08-16 04:25:14 +0800

    Add cors

--- api/app.js
+++ api/app.js
@@ -1,11 +1,28 @@
 require('app-module-path').addPath(require('app-root-path').toString());
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
 const HttpSuccess = require('api/responses/HttpSuccess.js');
 const app = express();
 
+const allowedOrigins = ['http://localhost:3000'];
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(
+    cors({
+      origin: function(origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+          const msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      },
+    })
+);
 
 // ROUTERS
 app.use('/api/ror/', require('api/routers/ReservationsRouter'));
@@ -21,4 +38,3 @@ app.use((req, res, next) => {
 app.listen(8080, () => {
   console.log(`Listening to port ${8080}`);
 });
-
